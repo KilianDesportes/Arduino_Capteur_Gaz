9409780d3be6561afee30aeb3775f870cd8d05ee
Kilian <kilian.desportes@gmail.com>
2020-12-02 15:17:48 +0100

    update with node, png

--- device_info/device_info.ino
+++ device_info/device_info.ino
@@ -5,8 +5,6 @@ SoftwareSerial loraSerial(10, 11);
 
 #define debugSerial Serial
 
-// Replace REPLACE_ME with TTN_FP_EU868 or TTN_FP_US915
-//#define freqPlan REPLACE_ME
 #define freqPlan TTN_FP_EU868
 
 TheThingsNetwork ttn(loraSerial, debugSerial, freqPlan);
@@ -17,17 +15,17 @@ void setup()
   debugSerial.begin(9600);
 
   String data = "";
-debugSerial.println("demarre");
-for (int i= 0; i<10 && data == ""; i++)
-{ 
-  loraSerial.write((byte)0x00);
-  loraSerial.write(0x55);
-  loraSerial.println();
-  loraSerial.println("sys get ver");
-  data = loraSerial.readStringUntil('\n');
-  delay(1000);
-} 
-debugSerial.println("fin demarre");  
+  debugSerial.println("demarre");
+  for (int i= 0; i<10 && data == ""; i++)
+  { 
+    loraSerial.write((byte)0x00);
+    loraSerial.write(0x55);
+    loraSerial.println();
+    loraSerial.println("sys get ver");
+    data = loraSerial.readStringUntil('\n');
+    delay(1000);
+  } 
+  debugSerial.println("fin demarre");  
 }
 
 void loop()
